4e2fcb15a971085544adaf3344e888593c2372c5
Aleksandar Dezelin <dezelin@gmail.com>
2015-05-05 18:11:43 +0200

    Blueprints library stubs.

---
+++ source/blueprints/BP_blueprintsettings.h
@@ -0,0 +1,39 @@
+/*
+* ***** BEGIN GPL LICENSE BLOCK *****
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+* The Original Code is Copyright (C) 2015 Aleksandar Dezelin.
+* All rights reserved.
+*
+* The Original Code is: all of this file.
+*
+* Contributor(s): none yet.
+*
+* ***** END GPL LICENSE BLOCK *****
+*/
+
+#ifndef __BP_BLUEPRINTSETTINGS_H__
+#define __BP_BLUEPRINTSETTINGS_H__
+
+typedef struct BP_BlueprintSettings {
+	int dummy;
+} BP_BlueprintSettings;
+
+BP_BlueprintSettings *BP_blueprint_settings_create();
+
+void BP_blueprint_settings_destroy(BP_BlueprintSettings *settings);
+
+#endif /* __BP_BLUEPRINTSETTINGS_H__ */
